9070e6a0e6a3226f6fa49798e12a26d1dd76facc
hernandezedca <hernandezedca@gmail.com>
2025-02-03 18:14:30 +0200

    Update contact-form.php

--- php/contact-form.php
+++ php/contact-form.php
@@ -1,49 +1,35 @@
 <?php
 
-if(isset($_POST['url']) && $_POST['url'] == '') { // Simple spam prevention
+// if the url field is empty
+if(isset($_POST['url']) && $_POST['url'] == '') {
 
-    // Put your email address here
-    $youremail = 'hernandezedca@gmail.com';
+	// put your email address here
+	$youremail = 'hernandezedca@gmail.com';  
 
-    // Sanitize and validate input
-    $name = htmlspecialchars(strip_tags($_POST['name']));
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $subject = htmlspecialchars(strip_tags($_POST['subject']));
-    $message = htmlspecialchars(strip_tags($_POST['message']));
+	// prepare message 
+	$body = "You have got a new message from the contact form on your website - RMVTherapy :
+	
+	Name:  $_POST[name]
+	Email:  $_POST[email]
+	Subject:  $_POST[subject]
+	Message:  $_POST[message]";
 
-    // Validate email format
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        die("Invalid email address.");
-    }
+	if( $_POST['email'] && !preg_match( "/[\r\n]/", $_POST['email']) ) {
+	  $headers = "From: $_POST[email]";
+	} else {
+	  $headers = "From: $youremail";
+	}
 
-    // Email headers
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+	mail($youremail, 'Message from RMV Therapy', $body, $headers );
 
-    // Email body
-    $body = "You have received a new message from the contact form on RMV Therapy:
+} ?>
 
-Name: $name
-Email: $email
-Subject: $subject
-
-Message:
-$message";
-
-    // Send email and check if successful
-    if(mail($youremail, 'Message from RMV Therapy', $body, $headers)) {
-        echo "<!DOCTYPE HTML>
-        <html>
-        <head><title>Thanks!</title></head>
-        <body><p>Thank you! We will get back to you soon.</p></body>
-        </html>";
-    } else {
-        echo "<!DOCTYPE HTML>
-        <html>
-        <head><title>Error</title></head>
-        <body><p>Sorry, your message could not be sent. Please try again later.</p></body>
-        </html>";
-    }
-}
-?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Thanks!</title>
+</head>
+<body>
+<p> Thank you! We will get back to you soon.</p>
+</body>
+</html>
